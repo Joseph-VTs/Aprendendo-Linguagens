02299e5a9ab490b65530cbc5c37b4283e5e93023
Joseph-VTs <compcopycode@gmail.com>
2026-07-29 17:42:03 -0300

    Limpando todo conteúdo entrarão Ideias Novas

--- ERROS.md
+++
@@ -1,5 +0,0 @@
-### Erros que aparecem
-- Network request failed
-- No route named "config" exists
-- 400 (Bad Request)
-- ERR_NAME_NOT_RESOLVED
--- meu-app-tarefas/app/(tabs)/config.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function ConfigScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Configurações do Perfil</Text>
-    </View>
-  );
-}
